36bb6c3702c6352ce2453fb162643e05b33167d8
MirindraRandrianarijaona <randrianarijaonamirindra@gmail.com>
2023-02-02 15:26:00 +0300

    modification du code python

--- server.py
+++ server.py
@@ -15,13 +15,26 @@ class index:
             pw='insigroup00',
             db='project00',
         )
-        a2=db.select('Album', limit=7)
+        a2=db.select('Album', limit=20)
+        artists=db.select('Artist', limit=20)
+        genres=db.select('Genre',limit=20)
         result = '<html><head><title>test</title></head>'
-        result += '<body>'
+        result += '<table border="1">'
+        result += '<tr><th>Genre</th><th>Artists</th><th>Album</th>'
         for a in a2:
-            result += a.Title + ',(' + str(a.ArtistId) + ') <br/>'
+            result +='<tr>'
+            for genre in genres:
+                result +='<td>'+genre.Name+'</td>'
+                break
+            for artist in artists:
+                result +='<td>'+artist.Name+'</td>'
+                break
+            result +='<td>'+a.Title+'</td>'
+            result +='</tr>'
+        result +='</table>'
         result += '</body></html>'
         return result
+        
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
