adf51215c7f97e156160b2dd0096b36a51fb86fe
Andriantsoa RATSIMANDRESY <andriantsoa024@gmail.com>
2023-02-15 11:51:07 +0200

    Création du fichier nav qui contient la navbar et qui sera importé par toutes les pages.

---
+++ nav.py
@@ -0,0 +1,15 @@
+def nav():
+    result = '<nav class="navbar navbar-expand-sm bg-light">'
+    result += '<div class="container-fluid">'
+    result += '<ul class="navbar-nav">'
+    result += '<li class="nav-item"><a class="nav-link" href="/index">Accueil</a></li>'
+    result += '<li class="nav-item"><a class="nav-link" href="/genre">Genre</a></li>'
+    result += '<li class="nav-item"><a class="nav-link" href="/artist">Artists</a></li>'
+    result += '<li class="nav-item"><a class="nav-link" href="/album">Album</a></li>'
+    result += '<li class="nav-item"><a class="nav-link" href="/track">Track</a></li>'
+    result += '<li class="nav-item"><a class="nav-link" href="/media">Media type</a></li>'
+    result += '<li class="nav-item"><a class="nav-link" href="/playlist">Playlist</a></li>'
+    result += '</ul>'
+    result += '</div>'
+    result += '</nav>'
+    return result
